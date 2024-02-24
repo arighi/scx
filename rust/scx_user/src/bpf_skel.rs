0588f1c1177e5a5aaeece4f4b16fed7b6d72ae65
Andrea Righi <andrea.righi@canonical.com>
2024-02-24 22:02:47 +0100

    scx_user: move scx_rustland backend code to scx_user
    Move the BPF component of scx_rustland to scx_user and make it available
    to other user-space schedulers.
    
    NOTE: main.bpf.c and bpf.rs are not pre-compiled in the scx_user crate,
    they need to be included in the user-space scheduler's source code in
    order to be compiled/linked properly.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>

--- rust/scx_user/src/bpf_skel.rs
+++ rust/scx_user/src/bpf_skel.rs
@@ -1,4 +1,4 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2.
 
-include!("../../../../rust/scx_user/src/bpf.rs");
+include!(concat!(env!("OUT_DIR"), "/bpf_skel.rs"));
--- scheds/rust/scx_rustland/build.rs
+++ scheds/rust/scx_rustland/build.rs
@@ -4,8 +4,8 @@
 fn main() {
     scx_utils::BpfBuilder::new()
         .unwrap()
-        .enable_intf("src/bpf/intf.h", "bpf_intf.rs")
-        .enable_skel("src/bpf/main.bpf.c", "bpf")
+        .enable_intf("../../../rust/scx_user/src/bpf/intf.h", "bpf_intf.rs")
+        .enable_skel("../../../rust/scx_user/src/bpf/main.bpf.c", "bpf")
         .build()
         .unwrap();
 }
