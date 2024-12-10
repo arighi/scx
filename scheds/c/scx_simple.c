189ad4a2a2e209891d9fd98068570a0b42293647
Andrea Righi <arighi@nvidia.com>
2024-12-10 08:55:11 +0100

    TEST: scx_simple: boost build workload
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- scheds/c/scx_simple.c
+++ scheds/c/scx_simple.c
@@ -19,7 +19,6 @@ const char help_fmt[] =
 "\n"
 "Usage: %s [-f] [-v]\n"
 "\n"
-"  -f            Use FIFO scheduling instead of weighted vtime scheduling\n"
 "  -v            Print libbpf debug messages\n"
 "  -h            Display this help and exit\n";
 
@@ -38,26 +37,6 @@ static void sigint_handler(int simple)
 	exit_req = 1;
 }
 
-static void read_stats(struct scx_simple *skel, __u64 *stats)
-{
-	int nr_cpus = libbpf_num_possible_cpus();
-	__u64 cnts[2][nr_cpus];
-	__u32 idx;
-
-	memset(stats, 0, sizeof(stats[0]) * 2);
-
-	for (idx = 0; idx < 2; idx++) {
-		int ret, cpu;
-
-		ret = bpf_map_lookup_elem(bpf_map__fd(skel->maps.stats),
-					  &idx, cnts[idx]);
-		if (ret < 0)
-			continue;
-		for (cpu = 0; cpu < nr_cpus; cpu++)
-			stats[idx] += cnts[idx][cpu];
-	}
-}
-
 int main(int argc, char **argv)
 {
 	struct scx_simple *skel;
@@ -73,9 +52,6 @@ restart:
 
 	while ((opt = getopt(argc, argv, "fvh")) != -1) {
 		switch (opt) {
-		case 'f':
-			skel->rodata->fifo_sched = true;
-			break;
 		case 'v':
 			verbose = true;
 			break;
@@ -88,14 +64,11 @@ restart:
 	SCX_OPS_LOAD(skel, simple_ops, scx_simple, uei);
 	link = SCX_OPS_ATTACH(skel, simple_ops, scx_simple);
 
-	while (!exit_req && !UEI_EXITED(skel, uei)) {
-		__u64 stats[2];
+	fprintf(stdout, "scheduler is running (CTRL+c to stop)\n");
+	fflush(stdout);
 
-		read_stats(skel, stats);
-		printf("local=%llu global=%llu\n", stats[0], stats[1]);
-		fflush(stdout);
+	while (!exit_req && !UEI_EXITED(skel, uei))
 		sleep(1);
-	}
 
 	bpf_link__destroy(link);
 	ecode = UEI_REPORT(skel, uei);
